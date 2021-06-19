2aad66f62b8686b658c773f74fe7b6f074bbe51e
Kyrncion <77999465+Kyrncion@users.noreply.github.com>
2021-06-18 22:57:13 -0700

    Update ActivityStatusScript.js

--- ActivityStatusScript.js
+++ ActivityStatusScript.js
@@ -1,18 +1,27 @@
-client.setInterval(() => {
+client.on("ready", async () => {
+	client.logger.info(`[READY] Logged in as ${client.user.tag}! ID: ${client.user.id}`)
+
+	client.activities.push(
+		{
+			text: () => `${formatNumber(client.guilds.cache.size)} servers`,
+			type: "WATCHING",
+		},
+		{
+			text: () =>
+				`with ${formatNumber(client.registry.commands.size)} commands`,
+			type: "PLAYING",
+		},
+		{
+			text: () => `${formatNumber(client.channels.cache.size)} channels`,
+			type: "WATCHING",
+		}
+	);
+
+	client.setInterval(() => {
 		const activity =
 			client.activities[Math.floor(Math.random() * client.activities.length)];
 		const text =
 			typeof activity.text === "function" ? activity.text() : activity.text;
 		client.user.setActivity(text, { type: activity.type });
 	}, 60000);
-
-	if (client.memePoster) {
-		client.setInterval(async () => {
-			try {
-				const post = await client.memePoster.fetchRandomPost(false);
-				await client.memePoster.post(post);
-			} catch (err) {
-				client.logger.error(err);
-			}
-		}, client.memePoster.postInterval);
-	}
+})
